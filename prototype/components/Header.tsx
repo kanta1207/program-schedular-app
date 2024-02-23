185ba638ce86c880ee2b205b8137c0f2a4231b87
shylabo <shuya.sohot@gmail.com>
2024-02-23 12:27:07 -0800

    update: navigation

--- prototype/components/Header.tsx
+++ prototype/components/Header.tsx
@@ -1,6 +1,33 @@
+'use client'
 import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
+
+const navigation = [
+  {
+    name: 'Schedules',
+    path: '/schedules',
+  },
+  {
+    name: 'Instructors',
+    path: '/instructors',
+  },
+  {
+    name: 'Enrollments',
+    path: '/enrollments',
+  },
+  {
+    name: 'Cohorts',
+    path: '/cohorts',
+  },
+  {
+    name: 'Courses',
+    path: '/courses',
+  },
+]
 
 const Header = () => {
+  const path = usePathname()
+  console.log(path)
   return (
     <header className="h-[60px] w-full flex items-center justify-between bg-primary px-4">
       <div className="flex items-center gap-6">
@@ -14,18 +41,13 @@ const Header = () => {
         {/* navigation */}
         <nav>
           <ul className="flex gap-x-4 text-muted">
-            <li>
-              <Link href={'/schedules'}>Schedules</Link>
-            </li>
-            <li>
-              <Link href={'/instructors'}>Instructors</Link>
-            </li>
-            <li>
-              <Link href={'/enrollments'}>Enrollments</Link>
-            </li>
-            <li>
-              <Link href={'/courses'}>Courses</Link>
-            </li>
+            {navigation.map((navItem) => (
+              <li key={navItem.path}>
+                <Link href={navItem.path} className={`${path === navItem.path && 'border-b'}`}>
+                  {navItem.name}
+                </Link>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
