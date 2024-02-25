13bf1024e854951bcbf06709e474b3f8f6d60ce1
shylabo <shuya.sohot@gmail.com>
2024-02-25 00:32:08 -0800

    add: constants

---
+++ frontend/constants/_index.ts
@@ -0,0 +1,6 @@
+export * from './classroom';
+export * from './contract';
+export * from './desired-working-hours';
+export * from './period-of-days';
+export * from './program';
+export * from './weekdays-range';
