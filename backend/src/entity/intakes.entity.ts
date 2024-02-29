27fe5c96af0b063ab5547070e65d577139e4d2cb
kanta1207 <kan0712livepool@gmail.com>
2024-02-29 13:55:05 -0800

    feat: create intakes entity

---
+++ backend/src/entity/intakes.entity.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('intakes')
+export class Intake {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @Column({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @Column({ name: 'start_at', type: 'timestamp' })
+  startAt: Date;
+
+  @Column({ name: 'end_at', type: 'timestamp' })
+  endAt: Date;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+}
