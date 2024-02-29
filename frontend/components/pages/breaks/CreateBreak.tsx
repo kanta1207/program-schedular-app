7d66af6bdeccaf1ec4731aa155222ca04d80bc73
shylabo <shuya.sohot@gmail.com>
2024-02-28 22:10:24 -0800

    add: create break

--- frontend/components/pages/breaks/CreateBreak.tsx
+++ frontend/components/pages/breaks/CreateBreak.tsx
@@ -1,55 +1,105 @@
 'use client';
 import { Dayjs } from 'dayjs';
 import { useState } from 'react';
+import { useForm, Controller, SubmitHandler, FieldValues } from 'react-hook-form';
 import Button from '@mui/material/Button';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { createBreak } from '@/actions/breaks/createBreak';
 
 const CreateBreak = () => {
   const [isCreating, setIsCreating] = useState(false);
-  const [startAt, setStartAt] = useState<Dayjs | null>(null);
-  const [endAt, setEndAt] = useState<Dayjs | null>(null);
+
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      startAt: null as Dayjs | null,
+      endAt: null as Dayjs | null,
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    try {
+      const payload = {
+        startAt: data.startAt,
+        endAt: data.endAt,
+      };
+
+      await createBreak(payload);
+
+      reset();
+      setIsCreating(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <div>
-      <div className="flex justify-end mb-4 ">
-        {!isCreating && (
-          <Button variant="contained" onClick={() => setIsCreating(!isCreating)}>
-            New Break
-          </Button>
-        )}
-      </div>
-
-      {isCreating && (
-        <div className="flex gap-4 items-end p-4 border my-4">
-          {/* Start Date */}
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={['DatePicker']}>
-              <DatePicker label="Start Date" value={startAt} onChange={(newDate) => setStartAt(newDate)} />
-            </DemoContainer>
-          </LocalizationProvider>
-          {/* End Date */}
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={['DatePicker']}>
-              <DatePicker label="End Date" value={endAt} onChange={(newDate) => setEndAt(newDate)} />
-            </DemoContainer>
-          </LocalizationProvider>
-          {/* Table Menu */}
-          <div className="flex items-end gap-4 ml-auto">
-            <Button
-              sx={{ display: 'flex', justifyContent: 'flex-end' }}
-              variant={'outlined'}
-              onClick={() => setIsCreating(!isCreating)}
-            >
-              Cancel
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex justify-end mb-4 ">
+          {!isCreating && (
+            <Button variant="contained" onClick={() => setIsCreating(!isCreating)}>
+              New Break
             </Button>
-            <Button variant={'contained'}>Create</Button>
-          </div>
+          )}
         </div>
-      )}
-    </div>
+
+        {isCreating && (
+          <div className="flex gap-4 items-end p-4 border my-4">
+            {/* Start Date */}
+            <Controller
+              control={control}
+              name="startAt"
+              rules={{ required: true }}
+              render={({ field }: any) => {
+                return (
+                  <DatePicker
+                    label="Start Date"
+                    value={field.value}
+                    inputRef={field.ref}
+                    onChange={(date) => field.onChange(date)}
+                  />
+                );
+              }}
+            />
+            {/* End Date */}
+            <Controller
+              control={control}
+              name="endAt"
+              rules={{ required: true }}
+              render={({ field }: any) => {
+                return (
+                  <DatePicker
+                    label="End Date"
+                    value={field.value}
+                    inputRef={field.ref}
+                    onChange={(date) => field.onChange(date)}
+                  />
+                );
+              }}
+            />
+            {/* Table Menu */}
+            <div className="flex items-end gap-4 ml-auto">
+              <Button
+                sx={{ display: 'flex', justifyContent: 'flex-end' }}
+                variant={'outlined'}
+                type="button"
+                onClick={() => {
+                  setIsCreating(!isCreating);
+                  reset();
+                }}
+              >
+                Cancel
+              </Button>
+              <Button variant={'contained'} type="submit">
+                Create
+              </Button>
+            </div>
+          </div>
+        )}
+      </form>
+    </LocalizationProvider>
   );
 };
 
