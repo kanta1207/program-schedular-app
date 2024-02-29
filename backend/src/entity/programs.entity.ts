0e0137c8146bb8cad149a4db5c4a316e00dd9b1d
kanta1207 <kan0712livepool@gmail.com>
2024-02-29 13:54:55 -0800

    fix: remove unnecessary `name` attr from programs

--- backend/src/entity/programs.entity.ts
+++ backend/src/entity/programs.entity.ts
@@ -5,6 +5,6 @@ export class Program {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ name: 'name', type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: 255 })
   name: string;
 }
