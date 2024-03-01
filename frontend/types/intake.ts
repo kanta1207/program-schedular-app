305eaf6640e2c50403c39cbef51a2a38c47294ff
shylabo <shuya.sohot@gmail.com>
2024-03-01 14:31:55 -0800

    update: intake type

--- frontend/types/intake.ts
+++ frontend/types/intake.ts
@@ -1,11 +1,42 @@
-import { Cohort } from './_index';
+import { PeriodOfDay, PeriodOfDayName, ClassroomName, ProgramName, Cohort } from './_index';
 
 export interface Intake {
   id: number;
+  name: string;
   startAt: Date;
   endAt: Date;
-  name: string;
-  cohorts: Cohort[];
-  createdAt?: Date;
-  updatedAt?: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface GetIntakesPeriodOfDay extends PeriodOfDay {
+  cohorts: {
+    id: number;
+    name: string;
+    createdAt: Date;
+    updatedAt: Date;
+  }[];
+}
+
+export interface GetIntakesResponse extends Intake {
+  periodOfDays: GetIntakesPeriodOfDay[];
+}
+
+export interface GetIntakeByIdResponse extends Intake {
+  cohorts: {
+    id: number;
+    name: string;
+    classroom: {
+      id: number;
+      name: ClassroomName;
+    };
+    program: {
+      id: number;
+      name: ProgramName;
+    };
+    periodOfDay: {
+      id: number;
+      name: PeriodOfDayName;
+    };
+  };
 }
