5c4587bdd6ff6503d93badbb838bef3ee6b147d5
koyaaoyama <aoyama.ko0920@gmail.com>
2024-02-29 18:39:05 -0800

    Fix main seeder. Add transaction, log

--- backend/src/seeders/main.seeder.ts
+++ backend/src/seeders/main.seeder.ts
@@ -2,22 +2,32 @@ import dataSource from '../data-source';
 import { masterClassroomSeeder } from './masterClassroom.seeder';
 import { masterContractTypeSeeder } from './masterContactType.seeder';
 import { masterPeriodOfDaySeeder } from './masterPeriodOfDay.seeder';
+import { masterWeekdaysRangeSeeder } from './masterWeekdaysRange.seeder';
 
 const mainSeeder = async () => {
+  await dataSource.initialize();
+  console.log('Data Source has been initialized.');
+
+  const queryRunner = dataSource.createQueryRunner();
+  await queryRunner.connect();
+
   try {
-    await dataSource.initialize();
-    console.log('Data Source has been initialized');
+    await queryRunner.startTransaction();
 
     console.log('Start seeding...');
     await masterClassroomSeeder();
     await masterContractTypeSeeder();
     await masterPeriodOfDaySeeder();
-    // TODO: other seeder
+    await masterWeekdaysRangeSeeder();
 
-    console.log('All seeding completed successfully.');
+    await queryRunner.commitTransaction();
+    console.log('All seeding completed successfully');
   } catch (error) {
+    await queryRunner.rollbackTransaction();
     console.error('Error during seeding: ', error);
   } finally {
+    await queryRunner.release();
+    console.log('Query Runner has been released');
     await dataSource.destroy();
     console.log('Data Source has been closed');
   }
