8636b7865a137219f622615b604ad976204f9875
shylabo <shuya.sohot@gmail.com>
2024-03-01 23:03:30 -0800

    chore: format json

--- backend/src/seeders/cohort.seeder.ts
+++ backend/src/seeders/cohort.seeder.ts
@@ -5,123 +5,63 @@ import { seederWrapper } from './utils';
 const cohorts = [
   {
     name: 'M-1023',
-    intake: {
-      id: 2,
-    },
-    periodOfDay: {
-      id: 1,
-    },
-    program: {
-      id: 1,
-    },
+    intake: { id: 2 },
+    periodOfDay: { id: 1 },
+    program: { id: 1 },
   },
   {
     name: 'A-1023',
-    intake: {
-      id: 2,
-    },
-    periodOfDay: {
-      id: 2,
-    },
-    program: {
-      id: 1,
-    },
+    intake: { id: 2 },
+    periodOfDay: { id: 2 },
+    program: { id: 1 },
   },
   {
     name: 'E1-1023',
-    intake: {
-      id: 2,
-    },
-    periodOfDay: {
-      id: 3,
-    },
-    program: {
-      id: 1,
-    },
+    intake: { id: 2 },
+    periodOfDay: { id: 3 },
+    program: { id: 1 },
   },
   {
     name: 'E2-1023',
-    intake: {
-      id: 2,
-    },
-    periodOfDay: {
-      id: 3,
-    },
-    program: {
-      id: 1,
-    },
+    intake: { id: 2 },
+    periodOfDay: { id: 3 },
+    program: { id: 1 },
   },
   {
     name: 'A1-0124',
-    intake: {
-      id: 4,
-    },
-    periodOfDay: {
-      id: 2,
-    },
-    program: {
-      id: 1,
-    },
+    intake: { id: 4 },
+    periodOfDay: { id: 2 },
+    program: { id: 1 },
   },
   {
     name: 'E1-0124',
-    intake: {
-      id: 4,
-    },
-    periodOfDay: {
-      id: 3,
-    },
-    program: {
-      id: 1,
-    },
+    intake: { id: 4 },
+    periodOfDay: { id: 3 },
+    program: { id: 1 },
   },
   {
     name: 'E2-0124',
-    intake: {
-      id: 4,
-    },
-    periodOfDay: {
-      id: 3,
-    },
-    program: {
-      id: 1,
-    },
+    intake: { id: 4 },
+    periodOfDay: { id: 3 },
+    program: { id: 1 },
   },
   {
     name: 'E-0923',
-    intake: {
-      id: 1,
-    },
-    periodOfDay: {
-      id: 3,
-    },
-    program: {
-      id: 2,
-    },
+    intake: { id: 1 },
+    periodOfDay: { id: 3 },
+    program: { id: 2 },
   },
   {
     name: 'E-1223',
-    intake: {
-      id: 3,
-    },
-    periodOfDay: {
-      id: 3,
-    },
-    program: {
-      id: 2,
-    },
+    intake: { id: 3 },
+    periodOfDay: { id: 3 },
+    program: { id: 2 },
   },
   {
     name: 'E-0324',
-    intake: {
-      id: 5,
-    },
-    periodOfDay: {
-      id: 3,
-    },
-    program: {
-      id: 2,
-    },
+    intake: { id: 5 },
+    periodOfDay: { id: 3 },
+    program: { id: 2 },
   },
 ].map((cohort, i) => {
   return { id: i + 1, ...cohort };
--- backend/src/seeders/instructor.seeder.ts
+++ backend/src/seeders/instructor.seeder.ts
@@ -8,269 +8,180 @@ const instructors = [
     isActive: true,
     desiredWorkingHours: 20,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Brian',
     isActive: true,
     desiredWorkingHours: 40,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Caio Franco',
     isActive: true,
     desiredWorkingHours: 20,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Diogo',
     isActive: true,
     desiredWorkingHours: 20,
     note: '',
-    contractType: {
-      id: 2,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 2 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Gaurav',
     isActive: true,
     desiredWorkingHours: 20,
     note: '',
-    contractType: {
-      id: 2,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 2 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Iman',
     isActive: true,
     desiredWorkingHours: 10,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Jana',
     isActive: true,
     desiredWorkingHours: 20,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Kwame',
     isActive: true,
     desiredWorkingHours: 10,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 2,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 2 },
   },
   {
     name: 'Mariam',
     isActive: true,
     desiredWorkingHours: 40,
     note: '',
-    contractType: {
-      id: 1,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 1 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Rafael',
     isActive: true,
     desiredWorkingHours: 40,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Rodrigo',
     isActive: true,
     desiredWorkingHours: 20,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Vedant',
     isActive: true,
     desiredWorkingHours: 20,
     note: '',
-    contractType: {
-      id: 2,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 2 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Amir',
     isActive: false,
     desiredWorkingHours: 10,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Andres Franco',
     isActive: false,
     desiredWorkingHours: 40,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Aswini',
     isActive: false,
     desiredWorkingHours: 10,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Brigitte',
     isActive: false,
     desiredWorkingHours: 30,
     note: '',
-    contractType: {
-      id: 1,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 1 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Cody',
     isActive: false,
     desiredWorkingHours: 10,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 2,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 2 },
   },
   {
     name: 'Daria Astanaeva',
     isActive: false,
     desiredWorkingHours: 10,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 3,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 3 },
   },
   {
     name: 'Elmira',
     isActive: false,
     desiredWorkingHours: 30,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 3,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 3 },
   },
   {
     name: 'Jasleen',
     isActive: false,
     desiredWorkingHours: 10,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 1 },
   },
   {
     name: 'Rafaela',
     isActive: false,
     desiredWorkingHours: 10,
     note: '',
-    contractType: {
-      id: 3,
-    },
-    weekdaysRange: {
-      id: 2,
-    },
+    contractType: { id: 3 },
+    weekdaysRange: { id: 2 },
   },
   {
     name: 'Tiago',
     isActive: false,
     desiredWorkingHours: 40,
     note: '',
-    contractType: {
-      id: 1,
-    },
-    weekdaysRange: {
-      id: 1,
-    },
+    contractType: { id: 1 },
+    weekdaysRange: { id: 1 },
   },
 ].map((instructor, i) => {
   return { id: i + 1, ...instructor };
 });
-
 export const instructorSeeder = async () => {
   await seederWrapper('instructors', async () => {
     const instructorRepo = dataSource.getRepository(Instructor);
