867b92cf86741a1623f7dd4f476c29f010e7781d
kanta1207 <kan0712livepool@gmail.com>
2024-03-05 15:36:29 -0800

    fix: set updateProgramDto to Partial extend

--- backend/src/modules/programs/dto/update-program.dto.ts
+++ backend/src/modules/programs/dto/update-program.dto.ts
@@ -1,3 +1,4 @@
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateProgramDto } from './create-program.dto';
 
-export class UpdateProgramDto extends CreateProgramDto {}
+export class UpdateProgramDto extends PartialType(CreateProgramDto) {}
