7c7f796d00cdca6e0f223a642c25ad69042e3c3d
shylabo <shuya.sohot@gmail.com>
2024-03-05 14:04:00 -0800

    update: remove try catch

--- backend/src/modules/intakes/intakes.service.ts
+++ backend/src/modules/intakes/intakes.service.ts
@@ -20,109 +20,89 @@ export class IntakesService {
   ) {}
 
   async create(createIntakeDto: CreateIntakeDto) {
-    try {
-      return await this.intakeRepository.save(createIntakeDto);
-    } catch (error) {
-      throw error;
-    }
+    return await this.intakeRepository.save(createIntakeDto);
   }
 
   async findAll() {
-    try {
-      const [masterPeriodOfDays, intakes] = await Promise.all([
-        this.masterPeriodOfDayRepository.find(),
-        this.intakeRepository.find({
-          order: {
-            id: 'DESC',
-          },
-          relations: ['cohorts.periodOfDay'],
-        }),
-      ]);
+    const [masterPeriodOfDays, intakes] = await Promise.all([
+      this.masterPeriodOfDayRepository.find(),
+      this.intakeRepository.find({
+        order: {
+          id: 'DESC',
+        },
+        relations: ['cohorts.periodOfDay'],
+      }),
+    ]);
 
-      const formattedIntakes = intakes.map(
-        ({ id, name, startAt, endAt, createdAt, updatedAt, cohorts }) => ({
-          id,
-          name,
-          startAt,
-          endAt,
-          createdAt,
-          updatedAt,
-          periodOfDays: masterPeriodOfDays.map((period) => ({
-            ...period,
-            cohorts: cohorts
-              .filter((cohort) => cohort.periodOfDay.id === period.id)
-              .map(({ id, name, createdAt, updatedAt }) => ({
-                id,
-                name,
-                createdAt,
-                updatedAt,
-              }))
-              .sort((a, b) => a.id - b.id),
-          })),
-        }),
-      );
+    const formattedIntakes = intakes.map(
+      ({ id, name, startAt, endAt, createdAt, updatedAt, cohorts }) => ({
+        id,
+        name,
+        startAt,
+        endAt,
+        createdAt,
+        updatedAt,
+        periodOfDays: masterPeriodOfDays.map((period) => ({
+          ...period,
+          cohorts: cohorts
+            .filter((cohort) => cohort.periodOfDay.id === period.id)
+            .map(({ id, name, createdAt, updatedAt }) => ({
+              id,
+              name,
+              createdAt,
+              updatedAt,
+            }))
+            .sort((a, b) => a.id - b.id),
+        })),
+      }),
+    );
 
-      return formattedIntakes;
-    } catch (error) {
-      throw error;
-    }
+    return formattedIntakes;
   }
 
   async findOne(id: number) {
-    try {
-      const intake = await this.intakeRepository.findOne({
-        where: {
-          id,
-        },
-        relations: ['cohorts.periodOfDay', 'cohorts.program'],
-      });
+    const intake = await this.intakeRepository.findOne({
+      where: {
+        id,
+      },
+      relations: ['cohorts.periodOfDay', 'cohorts.program'],
+    });
 
-      if (!intake) {
-        throw new NotFoundException('Intake Not Found');
-      }
-
-      return intake;
-    } catch (error) {
-      throw error;
+    if (!intake) {
+      throw new NotFoundException('Intake Not Found');
     }
+
+    return intake;
   }
 
   async update(id: number, updateIntakeDto: UpdateIntakeDto) {
-    try {
-      const { startAt, endAt } = updateIntakeDto;
-      const existingIntake = await this.intakeRepository.findOneBy({ id });
-      if (
-        (startAt && endAt && startAt.getTime() >= endAt.getTime()) ||
-        (startAt && startAt.getTime() >= existingIntake.endAt.getTime()) ||
-        (endAt && endAt.getTime() <= existingIntake.startAt.getTime())
-      ) {
-        throw new BadRequestException('endAt must be after startAt');
-      }
-
-      const updateResult = await this.intakeRepository.update(
-        id,
-        updateIntakeDto,
-      );
-
-      if (updateResult.affected === 0) {
-        throw new NotFoundException('Intake Not Found');
-      }
-
-      return this.intakeRepository.findOneBy({ id });
-    } catch (error) {
-      throw error;
+    const { startAt, endAt } = updateIntakeDto;
+    const existingIntake = await this.intakeRepository.findOneBy({ id });
+    if (
+      (startAt && endAt && startAt.getTime() >= endAt.getTime()) ||
+      (startAt && startAt.getTime() >= existingIntake.endAt.getTime()) ||
+      (endAt && endAt.getTime() <= existingIntake.startAt.getTime())
+    ) {
+      throw new BadRequestException('endAt must be after startAt');
     }
+
+    const updateResult = await this.intakeRepository.update(
+      id,
+      updateIntakeDto,
+    );
+
+    if (updateResult.affected === 0) {
+      throw new NotFoundException('Intake Not Found');
+    }
+
+    return this.intakeRepository.findOneBy({ id });
   }
 
   async remove(id: number) {
-    try {
-      const deleteResult = await this.intakeRepository.delete(id);
+    const deleteResult = await this.intakeRepository.delete(id);
 
-      if (deleteResult.affected === 0) {
-        throw new NotFoundException('Intake Not Found');
-      }
-    } catch (error) {
-      throw error;
+    if (deleteResult.affected === 0) {
+      throw new NotFoundException('Intake Not Found');
     }
   }
 }
