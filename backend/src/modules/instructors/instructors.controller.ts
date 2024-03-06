8991a850c0cf4dd1add30b847578c8ca37b8fb6a
kanta1207 <kan0712livepool@gmail.com>
2024-03-06 13:06:12 -0800

    feat: implement controller

--- backend/src/modules/instructors/instructors.controller.ts
+++ backend/src/modules/instructors/instructors.controller.ts
@@ -1,34 +1,77 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { InstructorsService } from './instructors.service';
 import { CreateInstructorDto } from './dto/create-instructor.dto';
 import { UpdateInstructorDto } from './dto/update-instructor.dto';
+import { StatusCodes } from 'src/common/status-code';
+import { ApiResponse } from 'src/common/api-response';
 
 @Controller('instructors')
 export class InstructorsController {
   constructor(private readonly instructorsService: InstructorsService) {}
 
   @Post()
-  create(@Body() createInstructorDto: CreateInstructorDto) {
-    return this.instructorsService.create(createInstructorDto);
+  async create(@Body() createInstructorDto: CreateInstructorDto) {
+    const instructor =
+      await this.instructorsService.create(createInstructorDto);
+
+    return ApiResponse.new(
+      instructor,
+      StatusCodes.STATUS_CREATED.code,
+      StatusCodes.STATUS_CREATED.message,
+    );
   }
 
   @Get()
-  findAll() {
-    return this.instructorsService.findAll();
+  async findAll() {
+    const instructors = await this.instructorsService.findAll();
+    return ApiResponse.new(
+      instructors,
+      StatusCodes.STATUS_OK.code,
+      StatusCodes.STATUS_OK.message,
+    );
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.instructorsService.findOne(+id);
+  async findOne(@Param('id') id: number) {
+    const instructor = await this.instructorsService.findOne(id);
+    return ApiResponse.new(
+      instructor,
+      StatusCodes.STATUS_OK.code,
+      StatusCodes.STATUS_OK.message,
+    );
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateInstructorDto: UpdateInstructorDto) {
-    return this.instructorsService.update(+id, updateInstructorDto);
+  async update(
+    @Param('id') id: number,
+    @Body() updateInstructorDto: UpdateInstructorDto,
+  ) {
+    const instructor = await this.instructorsService.update(
+      id,
+      updateInstructorDto,
+    );
+    return ApiResponse.new(
+      instructor,
+      StatusCodes.STATUS_OK.code,
+      StatusCodes.STATUS_OK.message,
+    );
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.instructorsService.remove(+id);
+  async remove(@Param('id') id: number) {
+    await this.instructorsService.remove(id);
+    return ApiResponse.new(
+      null,
+      StatusCodes.STATUS_NO_CONTENT.code,
+      StatusCodes.STATUS_NO_CONTENT.message,
+    );
   }
 }
