320526c51a7ca3a844ce204c7a279bdba504a6db
shylabo <shuya.sohot@gmail.com>
2024-03-06 23:08:22 -0800

    refactor: relation syntax

--- backend/src/modules/intakes/intakes.service.ts
+++ backend/src/modules/intakes/intakes.service.ts
@@ -30,7 +30,11 @@ export class IntakesService {
         order: {
           id: 'DESC',
         },
-        relations: ['cohorts.periodOfDay'],
+        relations: {
+          cohorts: {
+            periodOfDay: true,
+          },
+        },
       }),
     ]);
 
@@ -65,7 +69,12 @@ export class IntakesService {
       where: {
         id,
       },
-      relations: ['cohorts.periodOfDay', 'cohorts.program'],
+      relations: {
+        cohorts: {
+          periodOfDay: true,
+          program: true,
+        },
+      },
     });
 
     if (!intake) {
