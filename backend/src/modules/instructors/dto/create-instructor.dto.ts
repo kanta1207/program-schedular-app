7299df52e8e3b1fbc67939d5b646b62e3d84c4d6
kanta1207 <kan0712livepool@gmail.com>
2024-03-06 16:24:30 -0800

    feat: implement create-instructor DTO

--- backend/src/modules/instructors/dto/create-instructor.dto.ts
+++ backend/src/modules/instructors/dto/create-instructor.dto.ts
@@ -1,1 +1,44 @@
-export class CreateInstructorDto {}
+import {
+  IsArray,
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateInstructorDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  name: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  isActive: boolean;
+
+  @IsString()
+  note: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  desiredWorkingHours: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  contractTypeId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  weekdaysRangeId: number;
+
+  @IsArray()
+  @IsNumber({}, { each: true })
+  @IsNotEmpty()
+  courseIds: number[];
+
+  @IsArray()
+  @IsNumber({}, { each: true })
+  @IsNotEmpty()
+  periodOfDaysIds: number;
+}
