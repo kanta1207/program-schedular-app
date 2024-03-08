573825d1be08cd5cc355c8598026ea1af5713994
kanta1207 <kan0712livepool@gmail.com>
2024-03-08 15:35:59 -0800

    refactor: remove unnecessary order statement

--- backend/src/modules/cohorts/cohorts.service.ts
+++ backend/src/modules/cohorts/cohorts.service.ts
@@ -65,7 +65,6 @@ export class CohortsService {
     const cohort = await this.cohortRepository.findOne({
       where: { id },
       order: {
-        id: 'DESC',
         classes: { startAt: 'ASC', endAt: 'ASC' },
       },
       relations: {
