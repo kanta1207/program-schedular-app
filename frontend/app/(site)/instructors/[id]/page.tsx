13ad171c400f2320a021e6691c2ab4dded193da6
shylabo <shuya.sohot@gmail.com>
2024-03-09 14:09:25 -0800

    update: pages in instructor

--- frontend/app/(site)/instructors/[id]/page.tsx
+++ frontend/app/(site)/instructors/[id]/page.tsx
@@ -1,8 +1,8 @@
+import { Box, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import { getInstructorById } from '@/actions/instructors/getInstructorById';
-import CreateInstructor from '@/components/pages/instructors/InstructorInfoForm';
+import InstructorInfoForm from '@/components/pages/instructors/InstructorInfoForm';
 import { InstructorScheduleTable } from '@/components/pages/instructors/InstructorScheduleTable';
 import Headline from '@/components/partials/Headline';
-import { Box, ToggleButton, ToggleButtonGroup } from '@mui/material';
 
 interface PageProps {
   params: { id: string };
@@ -13,21 +13,23 @@ const page = async ({ params }: PageProps) => {
   const instructor = await getInstructorById(id);
 
   return (
-    <div className="w-full">
-      <div>
+    <>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'end', marginBottom: '2rem' }}>
         <Headline name="New Instructor" />
-        <CreateInstructor instructor={instructor} />
-      </div>
-
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'end', marginBottom: '1rem' }}>
-        <Headline name={`${instructor.name}'s Schedule:`} />
-        <ToggleButtonGroup color="primary" exclusive aria-label="Schedule Type">
-          <ToggleButton value="list">List</ToggleButton>
-          <ToggleButton value="gantt">Gantt</ToggleButton>
-        </ToggleButtonGroup>
       </Box>
-      <InstructorScheduleTable instructor={instructor} />
-    </div>
+      <InstructorInfoForm instructor={instructor} />
+      <br />
+      <Box>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'end', marginBottom: '1rem' }}>
+          <Headline name={`${instructor.name}'s Schedule`} />
+          <ToggleButtonGroup color="primary" exclusive aria-label="Schedule Type">
+            <ToggleButton value="list">List</ToggleButton>
+            <ToggleButton value="gantt">Gantt</ToggleButton>
+          </ToggleButtonGroup>
+        </Box>
+        <InstructorScheduleTable instructor={instructor} />
+      </Box>
+    </>
   );
 };
 
--- frontend/app/(site)/instructors/new/page.tsx
+++ frontend/app/(site)/instructors/new/page.tsx
@@ -1,12 +1,18 @@
-import CreateInstructor from '@/components/pages/instructors/InstructorInfoForm';
+import Box from '@mui/material/Box';
+import InstructorInfoForm from '@/components/pages/instructors/InstructorInfoForm';
 import Headline from '@/components/partials/Headline';
 
 const InstructorPage = () => {
   return (
-    <div className="w-full">
-      <Headline name="New Instructor" />
-      <CreateInstructor pageType={'new'} />
-    </div>
+    <>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'end', marginBottom: '2rem' }}>
+        <Headline name="New Instructor" />
+      </Box>
+      <InstructorInfoForm />
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'end', marginBottom: '2rem' }}>
+        <Headline name="Schedule" />
+      </Box>
+    </>
   );
 };
 
