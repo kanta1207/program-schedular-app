6718189b9db8bf68f0fe5a591bd308fecb291645
shylabo <shuya.sohot@gmail.com>
2024-03-11 00:23:42 -0700

    add: implement get classes module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,6 +9,7 @@ import { IntakesModule } from './modules/intakes/intakes.module';
 import { BreaksModule } from './modules/breaks/breaks.module';
 import { InstructorsModule } from './modules/instructors/instructors.module';
 import { CohortsModule } from './modules/cohorts/cohorts.module';
+import { ClassesModule } from './modules/classes/classes.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { CohortsModule } from './modules/cohorts/cohorts.module';
     ProgramsModule,
     InstructorsModule,
     CohortsModule,
+    ClassesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
