5608512c829e9b10b1784f077e0faf8d57fe40d9
koyaaoyama <aoyama.ko0920@gmail.com>
2024-03-14 00:14:58 -0700

    Integrate POST:/courses

---
+++ frontend/actions/courses/createCourse.ts
@@ -0,0 +1,36 @@
+import { ApiResponse } from '@/types/apiResponse';
+
+interface CreateCoursePayload {
+  name: string;
+  programId: number;
+  requiredHours: number;
+}
+
+export const createCourse = async (payload: CreateCoursePayload) => {
+  try {
+    if (!payload.name || !payload.programId || payload.requiredHours < 0) {
+      throw new Error("Something's wrong in the input data");
+    }
+
+    const baseUrl = `${process.env.NEXT_PUBLIC_API_URL}/courses`;
+
+    const response = await fetch(baseUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error: any) {
+    console.error(error);
+    throw new Error(`An error occurred: ${error.message}`);
+  }
+};
--- frontend/components/pages/courses/CreateCourse.tsx
+++ frontend/components/pages/courses/CreateCourse.tsx
@@ -1,14 +1,37 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Controller, FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { Button, TextField, MenuItem, FormControl, InputLabel } from '@mui/material';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { PROGRAMS } from '@/constants/_index';
+import { GetProgramsResponse } from '@/types/program';
+import { createCourse } from '@/actions/courses/createCourse';
+import { useRouter } from 'next/navigation';
 
-const CreateCourse = () => {
+interface CreateCourseProps {
+  programs?: GetProgramsResponse[];
+}
+
+interface CourseFormValues {
+  name: string;
+  programId: number;
+  requiredHours: string;
+}
+
+const CreateCourse: React.FC<CreateCourseProps> = ({ programs }) => {
   const [isCreating, setIsCreating] = useState(false);
   const [selectedProgram, setSelectedProgram] = useState('');
   const [hours, setHours] = useState('');
+  const router = useRouter();
+
+  const { control, handleSubmit, reset } = useForm<CourseFormValues>({
+    defaultValues: {
+      name: '',
+      programId: 0,
+      requiredHours: '',
+    },
+  });
 
   const handleHoursChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
@@ -22,6 +45,22 @@ const CreateCourse = () => {
     setSelectedProgram(event.target.value);
   };
 
+  const onSubmit: SubmitHandler<CourseFormValues> = async (data) => {
+    const payload = {
+      ...data,
+      requiredHours: Number(data.requiredHours),
+    };
+
+    try {
+      await createCourse(payload);
+      reset();
+      setIsCreating(false);
+      router.refresh();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <div className="flex justify-end mb-4 ">
@@ -33,14 +72,26 @@ const CreateCourse = () => {
       </div>
 
       {isCreating && (
-        <div className="flex gap-4 items-end p-4 border my-4">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex gap-4 items-end p-4 border my-4">
           <div>
-            <TextField
-              required
-              id="courseName"
-              label="Course Name"
-              placeholder="Enter course name"
-              sx={{ width: '20rem' }}
+            <Controller
+              control={control}
+              name="name"
+              rules={{ required: true }}
+              render={({ field }: any) => {
+                return (
+                  <TextField
+                    required
+                    id="courseName"
+                    label="Course Name"
+                    placeholder="Enter course name"
+                    sx={{ width: '20rem' }}
+                    value={field.value}
+                    inputRef={field.ref}
+                    onChange={(name) => field.onChange(name)}
+                  />
+                );
+              }}
             />
           </div>
 
@@ -48,41 +99,65 @@ const CreateCourse = () => {
             <InputLabel id="select-program" required>
               Program
             </InputLabel>
-            <Select
-              labelId="select-program"
-              id="select-program"
-              value={selectedProgram}
-              label="Program"
-              onChange={handleSelectProgram}
-              sx={{ width: '20rem' }}
-              required
-            >
-              {PROGRAMS.map((program) => (
-                <MenuItem key={program.id} value={program.name}>
-                  {program.name}
-                </MenuItem>
-              ))}
-            </Select>
+            <Controller
+              control={control}
+              name="programId"
+              rules={{ required: true }}
+              render={({ field }: any) => {
+                return (
+                  <Select
+                    labelId="select-program"
+                    id="select-program"
+                    name="programId"
+                    value={field.value}
+                    label="Program"
+                    onChange={handleSelectProgram}
+                    sx={{ width: '20rem' }}
+                    required
+                    {...field}
+                  >
+                    {programs?.map((program) => (
+                      <MenuItem key={program.id} value={program.id}>
+                        {program.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                );
+              }}
+            />
           </FormControl>
 
           <div>
-            <TextField
-              required
-              id="requiredHours"
-              label="Required Hours"
-              placeholder="60"
-              type="number"
-              value={hours}
-              sx={{ width: '20rem' }}
-              onChange={handleHoursChange}
-              inputProps={{
-                type: 'number',
-                min: 0,
-                max: 999,
-                maxLength: 3,
-                onInput: (e: React.ChangeEvent<HTMLInputElement>) => {
-                  e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, e.target.maxLength);
-                },
+            <Controller
+              control={control}
+              name="requiredHours"
+              rules={{
+                required: true,
+                pattern: { value: /^\d+$/, message: '' },
+              }}
+              render={({ field }: any) => {
+                return (
+                  <TextField
+                    required
+                    id="requiredHours"
+                    label="Required Hours"
+                    placeholder="60"
+                    type="number"
+                    value={field.value}
+                    inputRef={field.ref}
+                    sx={{ width: '20rem' }}
+                    onChange={(requiredHours) => field.onChange(requiredHours)}
+                    inputProps={{
+                      type: 'number',
+                      min: 0,
+                      max: 999,
+                      maxLength: 3,
+                      onInput: (e: React.ChangeEvent<HTMLInputElement>) => {
+                        e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, e.target.maxLength);
+                      },
+                    }}
+                  />
+                );
               }}
             />
           </div>
@@ -94,9 +169,11 @@ const CreateCourse = () => {
             >
               Cancel
             </Button>
-            <Button variant={'contained'}>Create</Button>
+            <Button type="submit" variant={'contained'}>
+              Create
+            </Button>
           </div>
-        </div>
+        </form>
       )}
     </div>
   );
