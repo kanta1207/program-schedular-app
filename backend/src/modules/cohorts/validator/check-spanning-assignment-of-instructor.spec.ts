ac564a689a2f751660fa8f5905dfa549c269e40f
kanta1207 <kan0712livepool@gmail.com>
2024-03-19 14:38:47 -0700

    fix: separate the check function from the service and write unit test

---
+++ backend/src/modules/cohorts/validator/check-spanning-assignment-of-instructor.spec.ts
@@ -0,0 +1,86 @@
+import { Class, Cohort, MasterPeriodOfDay } from 'src/entity';
+import {
+  AFTERNOON_PERIOD_OF_DAY_ID,
+  EVENING_PERIOD_OF_DAY_ID,
+  MORNING_PERIOD_OF_DAY_ID,
+} from '../../../common/constants/master.constant';
+import { checkSpanningAssignmentOfInstructor } from './check-spanning-assignment-of-instructor';
+
+describe('checkSpanningAssignmentOfInstructor', () => {
+  const morningPeriodOfDay: MasterPeriodOfDay = {
+    id: MORNING_PERIOD_OF_DAY_ID,
+  } as MasterPeriodOfDay;
+  const eveningPeriodOfDay: MasterPeriodOfDay = {
+    id: EVENING_PERIOD_OF_DAY_ID,
+  } as MasterPeriodOfDay;
+  const afternoonPeriodOfDay: MasterPeriodOfDay = {
+    id: AFTERNOON_PERIOD_OF_DAY_ID,
+  } as MasterPeriodOfDay;
+
+  const class1: Class = {
+    cohort: { periodOfDay: morningPeriodOfDay } as Cohort,
+    startAt: new Date('2022-01-01'),
+    endAt: new Date('2022-01-31'),
+  } as Class;
+
+  const class2: Class = {
+    cohort: { periodOfDay: eveningPeriodOfDay } as Cohort,
+    startAt: new Date('2022-02-01'),
+    endAt: new Date('2022-02-28'),
+  } as Class;
+
+  const class3: Class = {
+    cohort: { periodOfDay: afternoonPeriodOfDay } as Cohort,
+    startAt: new Date('2022-03-01'),
+    endAt: new Date('2022-03-31'),
+  } as Class;
+
+  const classesOfInstructor: Class[] = [class1, class2, class3];
+
+  it('should return null when the instructor is assigned to an afternoon class that overlaps with the new class', () => {
+    const periodOfDayOfCohort: MasterPeriodOfDay = afternoonPeriodOfDay;
+    const startAtOfClass = new Date('2022-03-15');
+    const endAtOfClass = new Date('2022-04-15');
+
+    const result = checkSpanningAssignmentOfInstructor(
+      periodOfDayOfCohort,
+      startAtOfClass,
+      endAtOfClass,
+      classesOfInstructor,
+    );
+
+    expect(result).toBeNull();
+  });
+
+  it('should return "Instructor is assigned to both Morning and Evening class in the same term" when the instructor is assigned to both Morning and Evening classes in the same term with the new class', () => {
+    const periodOfDayOfCohort: MasterPeriodOfDay = morningPeriodOfDay;
+    const startAtOfClass = new Date('2022-01-15');
+    const endAtOfClass = new Date('2022-02-15');
+
+    const result = checkSpanningAssignmentOfInstructor(
+      periodOfDayOfCohort,
+      startAtOfClass,
+      endAtOfClass,
+      classesOfInstructor,
+    );
+
+    expect(result).toBe(
+      'Instructor is assigned to both Morning and Evening class in the same term',
+    );
+  });
+
+  it('should return null when the instructor is not assigned to both Morning and Evening classes in the same term', () => {
+    const periodOfDayOfCohort: MasterPeriodOfDay = morningPeriodOfDay;
+    const startAtOfClass = new Date('2022-03-15');
+    const endAtOfClass = new Date('2022-04-15');
+
+    const result = checkSpanningAssignmentOfInstructor(
+      periodOfDayOfCohort,
+      startAtOfClass,
+      endAtOfClass,
+      classesOfInstructor,
+    );
+
+    expect(result).toBeNull();
+  });
+});
---
+++ backend/src/modules/cohorts/validator/check-spanning-assignment-of-instructor.ts
@@ -0,0 +1,57 @@
+import { Class, MasterPeriodOfDay } from 'src/entity';
+import {
+  AFTERNOON_PERIOD_OF_DAY_ID,
+  EVENING_PERIOD_OF_DAY_ID,
+  MORNING_PERIOD_OF_DAY_ID,
+} from '../../../common/constants/master.constant';
+
+// TODO: We might want to take `day of the week` into account when new data like `SAT-SUN` is introduced.
+/**
+ * @param periodOfDayOfCohort - Period of Day of the Cohort the instructor is being assigned to
+ * @param startAtOfClass - Start date of the Class the instructor is being assigned to
+ * @param endAtOfClass - End date of the Class the instructor is being assigned to
+ * @param classesOfInstructor - Classes the instructor is already assigned to
+ * @returns An alert message when the instructor is assigned to both Morning and Evening class in the same term, else null
+ */
+export const checkSpanningAssignmentOfInstructor = (
+  periodOfDayOfCohort: MasterPeriodOfDay,
+  startAtOfClass: Date,
+  endAtOfClass: Date,
+  classesOfInstructor: Class[],
+): string | null => {
+  /**
+   * If the instructor is assigned to an afternoon class that overlaps with the new class,
+   * We don't need to check for spanning assignment between morning and evening classes.
+   */
+  const hasOverlappingAfternoonClass = classesOfInstructor.some((clazz) => {
+    const { startAt, endAt } = clazz;
+    return (
+      clazz.cohort.periodOfDay.id === AFTERNOON_PERIOD_OF_DAY_ID &&
+      startAt <= endAtOfClass &&
+      endAt >= startAtOfClass
+    );
+  });
+
+  if (hasOverlappingAfternoonClass) {
+    return null;
+  }
+
+  const relevantClasses = classesOfInstructor.filter((clazz) => {
+    if (periodOfDayOfCohort.id === MORNING_PERIOD_OF_DAY_ID) {
+      return clazz.cohort.periodOfDay.id === EVENING_PERIOD_OF_DAY_ID;
+    }
+    if (periodOfDayOfCohort.id === EVENING_PERIOD_OF_DAY_ID) {
+      return clazz.cohort.periodOfDay.id === MORNING_PERIOD_OF_DAY_ID;
+    }
+    return false;
+  });
+
+  const overlappingClasses = relevantClasses.filter((clazz) => {
+    const { startAt, endAt } = clazz;
+    return startAt <= endAtOfClass && endAt >= startAtOfClass;
+  });
+  if (overlappingClasses.length > 0) {
+    return `Instructor is assigned to both Morning and Evening class in the same term`;
+  }
+  return null;
+};
