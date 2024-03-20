49638e37e12711a8a40950d561869d8462ca100e
kanta1207 <kan0712livepool@gmail.com>
2024-03-20 13:53:36 -0700

    refactor: remove unnecessary type annotation

--- backend/src/modules/cohorts/validator/check-spanning-assignment-of-instructor.spec.ts
+++ backend/src/modules/cohorts/validator/check-spanning-assignment-of-instructor.spec.ts
@@ -7,42 +7,38 @@ import {
 import { checkSpanningAssignmentOfInstructor } from './check-spanning-assignment-of-instructor';
 
 describe('checkSpanningAssignmentOfInstructor', () => {
-  const morningPeriodOfDay: MasterPeriodOfDay = {
+  const morningPeriodOfDay = {
     id: MORNING_PERIOD_OF_DAY_ID,
   } as MasterPeriodOfDay;
-  const eveningPeriodOfDay: MasterPeriodOfDay = {
+  const eveningPeriodOfDay = {
     id: EVENING_PERIOD_OF_DAY_ID,
   } as MasterPeriodOfDay;
-  const afternoonPeriodOfDay: MasterPeriodOfDay = {
+  const afternoonPeriodOfDay = {
     id: AFTERNOON_PERIOD_OF_DAY_ID,
   } as MasterPeriodOfDay;
 
-  const morningClass: Class = {
+  const morningClass = {
     cohort: { periodOfDay: morningPeriodOfDay } as Cohort,
     startAt: new Date('2022-01-01'),
     endAt: new Date('2022-01-31'),
   } as Class;
 
-  const eveningClass: Class = {
+  const eveningClass = {
     cohort: { periodOfDay: eveningPeriodOfDay } as Cohort,
     startAt: new Date('2022-02-01'),
     endAt: new Date('2022-02-28'),
   } as Class;
 
-  const afternoonClass: Class = {
+  const afternoonClass = {
     cohort: { periodOfDay: afternoonPeriodOfDay } as Cohort,
     startAt: new Date('2022-03-01'),
     endAt: new Date('2022-03-31'),
   } as Class;
 
-  const classesOfInstructor: Class[] = [
-    morningClass,
-    eveningClass,
-    afternoonClass,
-  ];
+  const classesOfInstructor = [morningClass, eveningClass, afternoonClass];
 
   it('should return null when the instructor is assigned to an afternoon class that overlaps with the new class', () => {
-    const periodOfDayOfCohort: MasterPeriodOfDay = afternoonPeriodOfDay;
+    const periodOfDayOfCohort = afternoonPeriodOfDay;
     const startAtOfClass = new Date('2022-03-15');
     const endAtOfClass = new Date('2022-04-15');
 
@@ -57,7 +53,7 @@ describe('checkSpanningAssignmentOfInstructor', () => {
   });
 
   it('should return message when the new class is in Morning, and the instructor is already assigned to Evening classes in the same term', () => {
-    const periodOfDayOfCohort: MasterPeriodOfDay = morningPeriodOfDay;
+    const periodOfDayOfCohort = morningPeriodOfDay;
     const startAtOfClass = new Date('2022-02-01');
     const endAtOfClass = new Date('2022-02-15');
 
@@ -74,7 +70,7 @@ describe('checkSpanningAssignmentOfInstructor', () => {
   });
 
   it('should return message when the new class is in Evening, and the instructor is already assigned to Morning classes in the same term', () => {
-    const periodOfDayOfCohort: MasterPeriodOfDay = eveningPeriodOfDay;
+    const periodOfDayOfCohort = eveningPeriodOfDay;
     const startAtOfClass = new Date('2022-01-01');
     const endAtOfClass = new Date('2022-01-15');
 
@@ -91,7 +87,7 @@ describe('checkSpanningAssignmentOfInstructor', () => {
   });
 
   it('should return null when the instructor is not assigned to both Morning and Evening classes in the same term', () => {
-    const periodOfDayOfCohort: MasterPeriodOfDay = morningPeriodOfDay;
+    const periodOfDayOfCohort = morningPeriodOfDay;
     const startAtOfClass = new Date('2022-03-15');
     const endAtOfClass = new Date('2022-04-15');
 
