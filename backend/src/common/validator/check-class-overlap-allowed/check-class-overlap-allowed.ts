bff054d2d27250d24e47a21682b414d9021bdb3c
koyaaoyama <aoyama.ko0920@gmail.com>
2024-03-23 12:35:27 -0700

    Separate functions in heck-class-overlap-allowed

--- backend/src/common/validator/check-class-overlap-allowed/check-class-overlap-allowed.ts
+++ backend/src/common/validator/check-class-overlap-allowed/check-class-overlap-allowed.ts
@@ -5,7 +5,36 @@ import {
   WEEKDAYS_RANGE_WED_FRI,
 } from '../../constants/master.constant';
 
-// checkClassOverlap
+/**
+ * Create error message from the Class instance
+ * ex) 'Overlaps with 2024-03-01 - 2024-03-20(Mon - Wed)'
+ *
+ * @param {FormattedClass} clazz
+ * @return {string}
+ */
+const generateMessage = (clazz: FormattedClass): string => {
+  const startAt = dayjs(clazz.startAt).format('YYYY-MM-DD');
+  const endAt = dayjs(clazz.startAt).format('YYYY-MM-DD');
+  const rangeName = clazz.weekdaysRange.data.name;
+
+  return `Overlaps with ${startAt} - ${endAt}(${rangeName})`;
+};
+
+/**
+ * Check if the overlap is allowed case or not
+ *
+ * @param {number} rangeAId id of WeekdaysRange
+ * @param {number} rangeBId id of WeekdaysRange
+ * @return {boolean} true if the overlap is allowed
+ */
+const checkOverlapAllowed = (rangeAId: number, rangeBId: number): boolean => {
+  const allowedCombinations = [
+    [WEEKDAYS_RANGE_MON_WED.id, WEEKDAYS_RANGE_WED_FRI.id],
+    [WEEKDAYS_RANGE_WED_FRI.id, WEEKDAYS_RANGE_MON_WED.id],
+  ];
+  // Allow overlaps of [Mon-Wed, Wed-Fri] or [Wed-Fri, Mon-Wed]
+  return allowedCombinations.some(([a, b]) => rangeAId === a && rangeBId === b);
+};
 
 /**
  * Check if there are any overlapped schedule in the given list of classes
@@ -16,25 +45,6 @@ import {
 export const checkClassOverlapAllowed = (
   classes: FormattedClass[],
 ): FormattedClass[] => {
-  const checkOverlapAllowed = (rangeAId: number, rangeBId: number) => {
-    const allowedCombinations = [
-      [WEEKDAYS_RANGE_MON_WED.id, WEEKDAYS_RANGE_WED_FRI.id],
-      [WEEKDAYS_RANGE_WED_FRI.id, WEEKDAYS_RANGE_MON_WED.id],
-    ];
-    // Allow overlaps of [Mon-Wed, Wed-Fri] or [Wed-Fri, Mon-Wed]
-    return allowedCombinations.some(
-      ([a, b]) => rangeAId === a && rangeBId === b,
-    );
-  };
-
-  const generateMessage = (clazz: FormattedClass) => {
-    const startAt = dayjs(clazz.startAt).format('YYYY-MM-DD');
-    const endAt = dayjs(clazz.startAt).format('YYYY-MM-DD');
-    const rangeName = clazz.weekdaysRange.data.name;
-
-    return `Overlaps with ${startAt} - ${endAt}(${rangeName})`;
-  };
-
   // Compare each classes
   for (let i = 0; i < classes.length; i++) {
     for (let j = i + 1; j < classes.length; j++) {
