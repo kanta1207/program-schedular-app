906a811710a2588d415a8a30ad33eefefb24b8d4
Kanayo <kanayo.uehara+github@gmail.com>
2024-04-09 14:12:54 -0700

    feat: add rquired mark in tables (breaks, courses, programs)

--- frontend/components/partials/RequiredMark.tsx
+++ frontend/components/partials/RequiredMark.tsx
@@ -7,3 +7,7 @@ interface RequiredMarkProps {
 export const RequiredMark: React.FC<RequiredMarkProps> = ({ isEditable }) => {
   return <span className={isEditable ? 'text-[#FF0000]' : ''}>{isEditable ? ' *' : ' :'}</span>;
 };
+
+export const TableRowRequiredMark: React.FC<RequiredMarkProps> = ({ isEditable }) => {
+  return <span className={isEditable ? 'text-[#FF0000]' : ''}>{isEditable ? ' *' : ''}</span>;
+};
